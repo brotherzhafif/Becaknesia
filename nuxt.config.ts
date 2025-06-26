dcf9211257067554b7e1c761e2bf012027f67728
brotherzhafif <bangz1504@gmail.com>
2025-06-26 17:15:04 +0700

    init: Becaknesia Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
